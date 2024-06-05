7d52fe5d03d99d887c77582c752dd32d23b30dff
DDoudine <amandinekemp@gmail.com>
2024-06-05 15:49:54 +0200

    Fix image height carousel

--- accueil.js
+++ accueil.js
@@ -1,16 +1,25 @@
-// Custom JS for handling any specific functionality
+$(document).ready(function() {
+  // Fonction pour ajuster la hauteur de l'élément .carousel-inner
+  function adjustCarouselHeight() {
+    var screenWidth = $(window).width();
+    var carouselHeight = 210; // Hauteur par défaut pour une feuille A4
 
-// Example: Toggle dropdown menu
-document.addEventListener('DOMContentLoaded', function() {
-  var menuToggle = document.getElementById('menu-toggle');
-  if (menuToggle) {
-    menuToggle.addEventListener('click', function() {
-        // Toggle the menu (e.g., open/close the sidebar or dropdown)
-        var dropdownMenu = document.getElementById('dropdown-menu');
-        if (dropdownMenu) {
-          dropdownMenu.classList.toggle('visible');
-          dropdownMenu.classList.toggle('hidden');
-        }
-    });
+    // Ajuster la hauteur en fonction de la largeur de l'écran
+    if (screenWidth < 768) { // Pour les écrans de moins de 768 px de largeur
+      carouselHeight = 150; // Réduire la hauteur à 150 px
+    } else if (screenWidth < 992) { // Pour les écrans de 768 px à 991 px de largeur
+      carouselHeight = 180; // Réduire la hauteur à 180 px
+    }
+
+    // Appliquer la nouvelle hauteur à l'élément .carousel-inner
+    $('.activities .carousel-inner').css('height', carouselHeight);
   }
+
+  // Appeler la fonction pour ajuster la hauteur lorsque la page est chargée
+  adjustCarouselHeight();
+
+  // Appeler la fonction pour ajuster la hauteur lorsque la taille de l'écran change
+  $(window).resize(function() {
+    adjustCarouselHeight();
+  });
 });
