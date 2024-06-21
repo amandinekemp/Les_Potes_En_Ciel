6252614202499d70914e33dc7bbd528d4ee16817
DDoudine <amandinekemp@gmail.com>
2024-06-21 10:14:06 +0200

    Convert Navbar with Bootstrap

--- app/_components/navbar/Navbar.tsx
+++ app/_components/navbar/Navbar.tsx
@@ -1,44 +1,35 @@
-import Image from "next/image";
-import Link from "next/link";
+import { DropdownDivider, DropdownItem, NavbarBrand, NavbarCollapse, NavbarToggle, NavLink } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Navbar = () => {
+function BasicExample() {
   return (
-    <>
-    <header className="container">
-      <div className="row">
-        <div className="col-md-1">
-          <a href="#" className="navbar-toggler" type="button" id="menu-toggle">
-            <i className="bi bi-list text-dark"></i>
-          </a>
-        </div>
-      
-        <div className="col-md-9">
-          {/* <div className="header banner_title"> */}
-            <Link href={"/"}><h1>Les Potes En Ciel</h1></Link>
-          {/* </div> */}
-        </div>
-
-        <div className="col-md-1">
-          <div className="d-flex align-items-center">
-            <Link href={"/login"} className="mr-3">
-              <i className="bi bi-person-circle text-dark"></i>
-            </Link>
-          </div>
-        </div>
-
-        <div className="col-md-1">
-          <a className="btn btn-link text-dark dropdown-toggle" href="#" role="button" id="languageDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i className="bi bi-globe"></i>
-          </a>
-          <div className="dropdown-menu" aria-labelledby="languageDropdown">
-            <a className="dropdown-item" href="#">Français</a>
-            <a className="dropdown-item" href="#">English</a>
-          </div>
-        </div>
-      </div>
-    </header>
-    </>
+    <Navbar expand="lg" className="bg-body-tertiary d-flex">
+      <Container>
+        <NavbarToggle aria-controls="basic-navbar-nav" />
+        <NavbarCollapse id="basic-navbar-nav">
+          <Nav>
+            <NavLink href="#home">Home</NavLink>
+            <NavLink href="#link">Link</NavLink>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <DropdownItem href="#action/3.1">Action</DropdownItem>
+              <DropdownItem href="#action/3.2">
+                Another action
+              </DropdownItem>
+              <DropdownItem href="#action/3.3">Something</DropdownItem>
+              <DropdownDivider />
+              <DropdownItem href="#action/3.4">
+                Separated link
+              </DropdownItem>
+            </NavDropdown>
+          </Nav>
+        </NavbarCollapse>
+        <NavbarBrand href="#home">React-Bootstrap</NavbarBrand>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default BasicExample;
