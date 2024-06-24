8b40ac9f338a3972b5b10aa8957cc8c3192e4818
DDoudine <amandinekemp@gmail.com>
2024-06-24 14:29:38 +0200

    Add components for News Feed

---
+++ app/(routes)/news_feed/page.tsx
@@ -0,0 +1,19 @@
+import NewsFeedCmpt from "@/app/_components/news_feed/NewsFeedCmpt";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "A propos de l'association - Les potes en ciel",
+  description: "A propos des potes en ciel",
+};
+
+export default function NewsFeedPage() {
+  return (
+    <>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <NewsFeedCmpt/>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ app/_components/news_feed/NewsFeedCmpt.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const NewsFeedCmpt = () => {
+  return (
+    <Container className="my-5">
+      <Row className="justify-content-center text-center">
+        <Col xs={10} md={8}>
+          <h2>Actualités</h2>
+          <p>Restez informé(e) des dernières nouvelles et événements des Potes en Ciel !</p>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col xs={10} md={8} className="d-flex justify-content-center">
+          {/* Insérer ici le visuel de la page Facebook */}
+          <iframe
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLesPotesEnCiel&tabs=timeline&width=500&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+            width="500"
+            height="600"
+            style={{ border: 'none', overflow: 'hidden' }}
+            scrolling="no"
+            frameBorder="0"
+            allowTransparency="true"
+            allow="encrypted-media"
+            title="Page Facebook Les Potes en Ciel"
+          ></iframe>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default NewsFeedCmpt;
