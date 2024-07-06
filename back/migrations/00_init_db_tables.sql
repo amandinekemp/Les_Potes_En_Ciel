30d3d6e3338d00d798a0b8526b1692a33bb2068a
DDoudine <amandinekemp@gmail.com>
2024-07-06 15:39:05 +0200

    Fix script sql

--- back/migrations/00_init_db_tables.sql
+++ back/migrations/00_init_db_tables.sql
@@ -27,40 +27,7 @@ CREATE TABLE IF NOT EXISTS `person` (
 
 -- Création de la table Book
 CREATE TABLE IF NOT EXISTS `book` (
-    `isbn` int(13) NOT NULL,
-    `title` varchar(255) NOT NULL,
-    `author` varchar(255) NOT NULL,
-    `summary` TEXT NOT NULL,
-    `is_available` BOOLEAN NOT NULL,
-    `genre_list` varchar(128) NOT NULL,
-    PRIMARY KEY (`isbn`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Création de la table Borrow
-CREATE TABLE IF NOT EXISTS `borrow` (
-    `id_borrow` int(11) NOT NULL AUTO_INCREMENT,
-    `isbn` int(13) NOT NULL,
-    `id_family` int(11) NOT NULL,
-    `loan_date` DATE NOT NULL,
-    `return_date` DATE NOT NULL,
-    PRIMARY KEY (`id_borrow`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Création de la table Workshop
-CREATE TABLE IF NOT EXISTS `workshop` (
-    `id_workshop` int(11) NOT NULL AUTO_INCREMENT,
-    `title` varchar(128) NOT NULL,
-    `start_date` DATETIME NOT NULL,
-    `end_date` DATETIME NOT NULL,
-    `description` TEXT NOT NULL,
-    `place` varchar(128) NOT NULL,
-    `birth_date` DATE NOT NULL,
-    PRIMARY KEY (`id_person`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Création de la table Book
-CREATE TABLE IF NOT EXISTS `book` (
-    `isbn` int(13) NOT NULL,
+    `isbn` char(13) NOT NULL,
     `title` varchar(255) NOT NULL,
     `author` varchar(255) NOT NULL,
     `summary` TEXT NOT NULL,
@@ -72,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `book` (
 -- Création de la table Borrow
 CREATE TABLE IF NOT EXISTS `borrow` (
     `id_borrow` int(11) NOT NULL AUTO_INCREMENT,
-    `isbn` int(13) NOT NULL,
+    `isbn` char(13) NOT NULL,
     `id_family` int(11) NOT NULL,
     `loan_date` DATE NOT NULL,
     `return_date` DATE NOT NULL,
@@ -98,23 +65,9 @@ CREATE TABLE IF NOT EXISTS `workshop_participant` (
     PRIMARY KEY (`id_workshop`, `id_person`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-    `id_organizer` int(11) NOT NULL,
-    PRIMARY KEY (`id_workshop`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Création de la table Workshop_participant
-CREATE TABLE IF NOT EXISTS `workshop_participant` (
-    `id_workshop` int(11) NOT NULL,
-    `id_person` int(11) NOT NULL,
-    PRIMARY KEY (`id_workshop`, `id_person`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
 -- Création de la table Newsletter
 CREATE TABLE IF NOT EXISTS `newsletter` (
     `email` varchar(128) NOT NULL,
     `subscribe` BOOLEAN NOT NULL,
     PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
--- back/migrations/01_init_data.sql
+++ back/migrations/01_init_data.sql
@@ -14,24 +14,29 @@ insert into `person` (`id_person`, `id_family`, `person_type`, `first_name`, `la
 
 -- Book
 
-INSERT INTO `book` (`book_isbn`, `title`, `author`, `summary`, `is_available`, `genre_list`) VALUES 
-(2317023634, 'Être père: Une place à prendre au quotidien', 'Olivier Foissac', 'S’il est évident que les pères s’investissent toujours plus dans leur parentalité, accueillir un enfant reste un défi considérable. En lutte constante contre l’éducation qu’ils ont reçue, les stéréotypes de genre ou bien tout simplement le regard de la société, les pères s’interrogent aujourd’hui plus que jamais.', TRUE, 'Livre-Santé-Bien-être-Puériculture-Paternité'),
-(9782845942486, 'La Naissance en bandes dessinées - Tome I - Découvrez vos super pouvoirs !', 'Lucile Gomez', 'Ce livre est inutile: ce qu’il explique, les femmes le savent déjà. Mais elles ne savent pas qu’elles savent. Leur cerveau conscient, intelligent, leur néocortex surdéveloppé d’être humain, rempli de peurs depuis des siècles, les poussent à accepter une surmédicalisation de la naissance. Considérées comme le « sexe faible », elles ont fini par croire qu’accoucher était forcément douloureux, dangereux, violent… et qu’elles en étaient incapables sans médecins.', TRUE, 'Livre-BDEtHumour'),
-(9782957698929, 'Le petit illustré des conceptions, de l&aposadoption, des familles, de la parentalité, etc.', 'Tiphaine Dieumegard', 'Les petits illustrés de l’intimité sont les tout premiers livres engagés, inclusifs et sans tabou pour parler aux enfants d’anatomie et d’intimité. Ce troisième tome est composé de deux parties : Une partie anatomique et scientifique (avec de la fécondation, le développement du bébé, la gémellité, les différentes techniques de PMA, la naissance, l’allaitement, l’adoption, le deuil périnatal, etc.) Et une partie plus sociologique (avec les différentes familles, l’éducation, les droits de l’enfant, etc.) Ainsi qu’un lexique et une page listant les contacts d’associations.Ce tome a également reçu le label de la chaire UNESCO santé sexuelle et droits humains.', TRUE, 'Livre-LivreJeunesse'),
-(9782092783726, 'Le sommeil conseils et astuces au quotidien', 'Madeleine Deny', 'Un livre futé, truffé de conseils pour aider les parents à surmonter les problèmes de sommeil de leur enfant. Pour chaque âge, des situations quotidiennes analysées pour dédramatiser et bien réagir. Des astuces et des conseils &apostestés et approuvés&apos pour trouver ses solutions. Les réponses précises d&aposexperts à toutes vos questions... sans oublier beaucoup d&aposhumour et de tendresse, les meilleurs alliés d&aposune vie de famille réussie !', TRUE, 'Livre-Santé-LeSommeil');
+INSERT INTO `book` (`isbn`, `title`, `author`, `summary`, `available`, `genre_list`) VALUES 
+('9782317023637', 'Être père: Une place à prendre au quotidien', 'Olivier Foissac', 'S’il est évident que les pères s’investissent toujours plus dans leur parentalité, accueillir un enfant reste un défi considérable. En lutte constante contre l’éducation qu’ils ont reçue, les stéréotypes de genre ou bien tout simplement le regard de la société, les pères s’interrogent aujourd’hui plus que jamais.', TRUE, 'Livre-Santé-Bien-être-Puériculture-Paternité'),
+('9782845942486', 'La Naissance en bandes dessinées - Tome I - Découvrez vos super pouvoirs !', 'Lucile Gomez', 'Ce livre est inutile: ce qu’il explique, les femmes le savent déjà. Mais elles ne savent pas qu’elles savent. Leur cerveau conscient, intelligent, leur néocortex surdéveloppé d’être humain, rempli de peurs depuis des siècles, les poussent à accepter une surmédicalisation de la naissance. Considérées comme le « sexe faible », elles ont fini par croire qu’accoucher était forcément douloureux, dangereux, violent… et qu’elles en étaient incapables sans médecins.', TRUE, 'Livre-BDEtHumour'),
+('9782957698929', 'Le petit illustré des conceptions, de l&aposadoption, des familles, de la parentalité, etc.', 'Tiphaine Dieumegard', 'Les petits illustrés de l’intimité sont les tout premiers livres engagés, inclusifs et sans tabou pour parler aux enfants d’anatomie et d’intimité. Ce troisième tome est composé de deux parties : Une partie anatomique et scientifique (avec de la fécondation, le développement du bébé, la gémellité, les différentes techniques de PMA, la naissance, l’allaitement, l’adoption, le deuil périnatal, etc.) Et une partie plus sociologique (avec les différentes familles, l’éducation, les droits de l’enfant, etc.) Ainsi qu’un lexique et une page listant les contacts d’associations.Ce tome a également reçu le label de la chaire UNESCO santé sexuelle et droits humains.', TRUE, 'Livre-LivreJeunesse'),
+('9782092783726', 'Le sommeil conseils et astuces au quotidien', 'Madeleine Deny', 'Un livre futé, truffé de conseils pour aider les parents à surmonter les problèmes de sommeil de leur enfant. Pour chaque âge, des situations quotidiennes analysées pour dédramatiser et bien réagir. Des astuces et des conseils &apostestés et approuvés&apos pour trouver ses solutions. Les réponses précises d&aposexperts à toutes vos questions... sans oublier beaucoup d&aposhumour et de tendresse, les meilleurs alliés d&aposune vie de famille réussie !', TRUE, 'Livre-Santé-LeSommeil');
 
 -- Borrow
 
-INSERT INTO `borrow` (`book_isbn`, `id_family`, `loan_date`, `return_date`) VALUES 
-(2317023634, 1, '2024-07-03', '2024-07-24'),
-(9782845942486, 2, '2024-07-05', '2024-07-26');
+INSERT INTO `borrow` (`isbn`, `id_family`, `loan_date`, `return_date`) VALUES 
+('9782317023637', 1, '2024-07-03', '2024-07-24'),
+('9783161484100', 1, '2023-01-15', '2023-02-15'),
+('9783161484101', 2, '2023-02-20', '2023-03-20'),
+('9783161484102', 3, '2023-03-25', '2023-04-25'),
+('9783161484103', 4, '2023-04-30', '2023-05-30'),
+('9783161484104', 5, '2023-05-05', '2023-06-05'),
+('9782845942486', 2, '2024-07-05', '2024-07-26');
 
 -- Workshop
 
 INSERT INTO `workshop` (`id_workshop`, `title`, `start_date`, `end_date`, `description`, `place`, `id_organizer`) VALUES 
 (1, 'Ludothéque Parc Matisse', '2024-07-10 14:00:00', '2024-07-10 18:00:00', '', 'Parc Matisse avec le festival "Un air d&aposété".', 1),
-(2, 'Pote mobile', '2024-07-10 16:00:00', '2024-07-17:30:00', '', 'Place Caulier Lille', 2),
-(3, 'Pote mobile', '2024-07-12 16:00:00', '2024-07-17:30:00', '', 'Ecole Dombrowski à Hellemmes', 2);
+(2, 'Pote mobile', '2024-07-10 16:00:00', '2024-07-10 17:30:00', '', 'Place Caulier Lille', 2),
+(3, 'Pote mobile', '2024-07-12 16:00:00', '2024-07-12 17:30:00', '', 'Ecole Dombrowski à Hellemmes', 2);
 
 
 -- Workshop_participant
@@ -47,5 +52,3 @@ INSERT INTO `workshop_participant` (`id_workshop`, `id_person`) VALUES
 INSERT INTO `newsletter` (`email`, `subscribe`) VALUES
 ('amandinekemp+jaisouscrit@gmail.com', TRUE),
 ('amandinekemp+jaipassouscrit@gmail.com', FALSE);
-
-
