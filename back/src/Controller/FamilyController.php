e4b06d54f2ae82a2c7fa3094128fd0a52ed1cdc1
DDoudine <amandinekemp@gmail.com>
2024-07-06 17:51:53 +0200

    Add controller family

--- back/src/Controller/FamilyController.php
+++ back/src/Controller/FamilyController.php
@@ -2,18 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Family;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class FamilyController extends AbstractController
 {
-    #[Route('/family', name: 'app_family')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/FamilyController.php',
-        ]);
+  #[Route('/api/family', name: "createFamily", methods: ['POST'])]
+  public function createFamily(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, ValidatorInterface $validator): JsonResponse
+  {
+    $family = $serializer->deserialize($request->getContent(), Family::class, 'json');
+
+    // On vérifie les erreurs
+    $errors = $validator->validate($family);
+    if ($errors->count() > 0) {
+      return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
     }
+
+    $em->persist($family);
+    $em->flush();
+
+    $jsonFamily = $serializer->serialize($family, 'json');
+
+    return new JsonResponse($jsonFamily, Response::HTTP_CREATED, [], true);
+  }
+
+  #[Route('/api/family/{id}', name: "updateFamily", methods: ['PUT'])]
+  public function updateFamily(Family $bddFamily, Request $request, SerializerInterface $serializer, EntityManagerInterface $em, ValidatorInterface $validator): JsonResponse
+  {
+    $updatedFamily = $serializer->deserialize(
+      $request->getContent(),
+      Family::class,
+      'json',
+      [AbstractNormalizer::OBJECT_TO_POPULATE => $bddFamily]
+    );
+
+    // On vérifie les erreurs
+    $errors = $validator->validate($updatedFamily);
+    if ($errors->count() > 0) {
+      return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+    }
+
+    $em->persist($updatedFamily);
+    $em->flush();
+    return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+  }
 }
