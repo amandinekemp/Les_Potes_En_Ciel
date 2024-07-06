7a0ee8e68abf16f941fa0d6006ec0df24172b121
DDoudine <amandinekemp@gmail.com>
2024-07-06 11:57:05 +0200

    Format files and error message

--- back/src/Controller/BookController.php
+++ back/src/Controller/BookController.php
@@ -17,14 +17,14 @@ use App\Repository\BookRepository;
 
 class BookController extends AbstractController
 {
-    #[Route('/api/books', name: 'bookList', methods: ['GET'])]
-    public function getBookList(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
-    {
-      $bookList = $bookRepository->findAll();
+  #[Route('/api/books', name: 'bookList', methods: ['GET'])]
+  public function getBookList(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
+  {
+    $bookList = $bookRepository->findAll();
 
-      $jsonBookList = $serializer->serialize($bookList, 'json');
-      return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
-    }
+    $jsonBookList = $serializer->serialize($bookList, 'json');
+    return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
+  }
 
   //   #[Route('/api/books/{isbn}', name: 'bookDetail', methods: ['GET'])]
   //   public function getBookDetail(int $isbn, BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
@@ -37,44 +37,46 @@ class BookController extends AbstractController
   //       return new JsonResponse(null, Response::HTTP_NOT_FOUND);
   //  }
 
-    #[Route('/api/books/{id}', name: 'bookDetail', methods: ['GET'])]
-    public function getBookDetail(Book $bddBook, SerializerInterface $serializer): JsonResponse 
-    {
-      $jsonBook = $serializer->serialize($bddBook, 'json');
-      return new JsonResponse($jsonBook, Response::HTTP_OK, ['accept' => 'json'], true);
-    }
+  #[Route('/api/books/{id}', name: 'bookDetail', methods: ['GET'])]
+  public function getBookDetail(Book $bddBook, SerializerInterface $serializer): JsonResponse
+  {
+    $jsonBook = $serializer->serialize($bddBook, 'json');
+    return new JsonResponse($jsonBook, Response::HTTP_OK, ['accept' => 'json'], true);
+  }
 
-    #[Route('/api/books', name:"createBook", methods: ['POST'])]
-    public function createBook(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse 
-    {
-      $book = $serializer->deserialize($request->getContent(), Book::class, 'json');
-      $em->persist($book);
-      $em->flush();
+  #[Route('/api/books', name: "createBook", methods: ['POST'])]
+  public function createBook(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
+  {
+    $book = $serializer->deserialize($request->getContent(), Book::class, 'json');
+    $em->persist($book);
+    $em->flush();
 
-      $jsonBook = $serializer->serialize($book, 'json');
-      
-      return new JsonResponse($jsonBook, Response::HTTP_CREATED, [], true);
-    }
+    $jsonBook = $serializer->serialize($book, 'json');
 
-    #[Route('/api/books/{id}', name:"updateBook", methods:['PUT'])]
-    public function updateBook(Book $bddBook, Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse 
-    {
-        $updatedBook = $serializer->deserialize($request->getContent(), 
-                Book::class, 
-                'json', 
-                [AbstractNormalizer::OBJECT_TO_POPULATE => $bddBook]);
-        
-        $em->persist($updatedBook);
-        $em->flush();
-        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
-   }
-    
-    #[Route('/api/books/{id}', name: 'deleteBook', methods: ['DELETE'])]
-    public function deleteBook(Book $bddBook, EntityManagerInterface $em): JsonResponse 
-    {
-      $em->remove($bddBook);
-      $em->flush();
+    return new JsonResponse($jsonBook, Response::HTTP_CREATED, [], true);
+  }
 
-      return new JsonResponse(null, Response::HTTP_NO_CONTENT);
-    }
+  #[Route('/api/books/{id}', name: "updateBook", methods: ['PUT'])]
+  public function updateBook(Book $bddBook, Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
+  {
+    $updatedBook = $serializer->deserialize(
+      $request->getContent(),
+      Book::class,
+      'json',
+      [AbstractNormalizer::OBJECT_TO_POPULATE => $bddBook]
+    );
+
+    $em->persist($updatedBook);
+    $em->flush();
+    return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+  }
+
+  #[Route('/api/books/{id}', name: 'deleteBook', methods: ['DELETE'])]
+  public function deleteBook(Book $bddBook, EntityManagerInterface $em): JsonResponse
+  {
+    $em->remove($bddBook);
+    $em->flush();
+
+    return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+  }
 }
